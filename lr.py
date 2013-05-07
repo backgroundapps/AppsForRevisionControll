db8ce780561abd03ebf73da0c3d796c1124850bb
Jonatas Emidio de Souza <jonatasemidio@gmail.com>
2013-05-07 16:40:05 -0300

    Update lr.py

--- lr.py
+++ lr.py
@@ -1,12 +1,14 @@
 import os
 file_name = raw_input('Inserir o nome do arquivo a ser gerado: ')
 
+
 def open_file_to_get_the_last_revision(f):
 	new_file = open(f)
 	l = new_file.readlines(0)[0]
 	new_file.close()
 	return l
-
+	
+#this part of the code will select the files and find the head to prepare de process
 def prepare_file():	
 	if os.path.exists(file_name+'.txt'):
 		os.remove(file_name+'.txt')
